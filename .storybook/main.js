4ce79b6ccb89c6cca7c432da6423add18587fd84
Philipp Schardt <philipp.schardt@ergosign.de>
2020-03-25 17:53:27 +0100

    update dep

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,25 +4,6 @@ module.exports = {
   stories: ['../src/**/*.stories.ts'],
   presets: [
     "storybook-addon-pseudo-states-angular/preset-postcss"
-    // tests:
-    // {
-    //   name: ""storybook-addon-pseudo-states-angular/preset-postcss"",
-    //   options: {
-    //     postCSSDefaultOptions: {
-    //       prefix: 'test--'
-    //     }
-    //   }
-    // }
-    // path.resolve('./.storybook/my-preset')
-    // {
-    //   name: path.resolve('./.storybook/my-preset'),
-    //   // options: {
-    //   //   cssLoaderOptions: {
-    //   //     modules: true,
-    //   //     localIdentName: '[path][name]__[local]',
-    //   //   }
-    //   // }
-    // }
   ],
   addons: [
     '@storybook/addon-actions',
@@ -30,21 +11,4 @@ module.exports = {
     '@storybook/addon-notes',
     "storybook-addon-pseudo-states-angular/register",  // enable toolbar button
   ],
-  // tests
-  /*webpackFinal: (webpackConfig, { options }) => {
-    const { module = {} } = webpackConfig;
-    const {
-      postCSSDefaultOptions,
-      cssLoaderOptions,
-      sassLoaderOptions,
-      rule = {},
-    } = options;
-
-    console.log({cssLoaderOptions});
-    console.log({postCSSDefaultOptions});
-    console.log(options);
-    // return webpackConfig;
-    return null;
-  }*/
-  // webpackFinal: (config) => console.dir(config, { depth: null }) || config,
 };
