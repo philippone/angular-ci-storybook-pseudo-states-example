bef5e57c18af533fe36e50026f920bca483296ea
Philipp Schardt <philipp.schardt@ergosign.de>
2020-08-13 01:25:38 +0200

    added some options

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  stories: ['../src/**/*.stories.ts'],
+  stories: ["../src/**/*.stories.ts"],
   presets: [
-    "@ergosign/storybook-addon-pseudo-states-angular/preset-postcss"
+    {
+      name: "@ergosign/storybook-addon-pseudo-states-angular/preset-postcss",
+      // options: {
+      //   postCssLoaderPseudoClassesPluginOptions: {
+      //     // prefix: 'my-custom-pseudo-sates--',
+      //     //blacklist: [":nth-child", ":nth-of-type", ":test"],
+      //   },
+      // },
+    },
   ],
   addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-notes',
-    '@storybook/addon-knobs',
-    '@storybook/addon-viewport',
-    "@ergosign/storybook-addon-pseudo-states-angular/register",  // enable toolbar button
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-notes",
+    "@storybook/addon-knobs",
+    "@storybook/addon-viewport",
+    "@ergosign/storybook-addon-pseudo-states-angular/register", // enable toolbar button
   ],
 };
--- src/app/components/buttons/simple-button/simple-button.stories.ts
+++ src/app/components/buttons/simple-button/simple-button.stories.ts
@@ -1,17 +1,26 @@
-import { SimpleButtonComponent } from './simple-button.component';
-import { AttributesStatesDefault, PseudoStatesDefault, withPseudo } from '@ergosign/storybook-addon-pseudo-states-angular';
-import { boolean, withKnobs } from '@storybook/addon-knobs';
+import { SimpleButtonComponent } from "./simple-button.component";
+import {
+  AttributesStatesDefault,
+  PseudoStatesDefault,
+  withPseudo,
+  Orientation,
+} from "@ergosign/storybook-addon-pseudo-states-angular";
+import { boolean, withKnobs } from "@storybook/addon-knobs";
+import { StoryFn } from "@storybook/addons";
 
 export default {
-  title: 'Simple Button',
+  title: "Simple Button",
   decorators: [withPseudo, withKnobs],
   parameters: {
     withPseudo: {
-      selector: 'button',
-      pseudos: [...PseudoStatesDefault, 'focus & hover'],
-      attributes: [...AttributesStatesDefault, 'big']
-    }
-  }
+      selector: "button",
+      pseudos: [...PseudoStatesDefault, "focus & hover"],
+      attributes: [...AttributesStatesDefault, "big"],
+      styles: {
+        orientation: Orientation.COLUMN,
+      },
+    },
+  },
 };
 
 export const simpleButton = () => ({
@@ -21,14 +30,13 @@ export const simpleButton = () => ({
   //   imports: []
   // },
   props: {
-    label: 'Label',
-    rounded: boolean('rounded Corners', false)
-  }
+    label: "Label",
+    rounded: boolean("rounded Corners", false),
+  },
 });
-
 
 export const Story = () => {
   return {
-    component: SimpleButtonComponent
+    component: SimpleButtonComponent,
   };
 };
