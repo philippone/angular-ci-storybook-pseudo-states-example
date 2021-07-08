8be9c527004ba1493a2ecda4a1cef28994d57276
Philipp Schardt <philipp.schardt@ergosign.de>
2021-07-08 10:31:21 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonsModule } from './components/buttons/buttons.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ButtonsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
